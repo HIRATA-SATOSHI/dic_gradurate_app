8c0c3fb62ee464fa929d7c8f4b1215d6d6005700
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-12 00:15:37 +0900

    rails g controllerでnew,index,show,edit作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'students/new'
+  get 'students/index'
+  get 'students/show'
+  get 'students/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
