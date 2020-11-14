f9ee11e7c8683d5169668a5c64ea0e9b8253793a
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-14 11:00:46 +0900

    routingをresourcesへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'students/new'
-  get 'students/index'
-  get 'students/show'
-  get 'students/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'students/new'
+  # get 'students/index'
+  # get 'students/show'
+  # get 'students/edit'
+resources :students
 end
