b6272c76949a81e5b56e7e12db679f9532a836e3
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-14 21:23:55 +0900

    新規登録View項目 Bootstrap

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,26 @@ class Student < ApplicationRecord
     scholars: 4,
     liberal_arts: 5
   }
+
+  enum enrollment_status: {Yes: 0,
+    No: 1,
+    Break: 2
+  }
+
+  enum site: {自由が丘: 0,
+    渋谷: 1,
+    吉祥寺: 2,
+    四谷: 3,
+    酒田: 4
+  }
+
+  enum school_year: {小5: 0,
+    小6: 1,
+    中1: 2,
+    中2: 3,
+    中3: 4,
+    高1: 5,
+    高2: 6,
+    高3: 7
+  }
 end
