0ea041e38313b37436855bfa831c05e35dc9bddb
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-24 01:15:13 +0900

    controller修正完了

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,26 +1,55 @@
 class StudentsController < ApplicationController
-  def new
-    @student = Student.new
-  end
-
-  def create
-
-    @student = Student.new(student_params)
-    redirect_to students_path
-  end
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   def index
-   @students= Student.all
+    @students= Student.all
   end
 
   def show
   end
 
+  def new
+    @student = Student.new
+  end
+
   def edit
   end
+
+  def create
+    @student = Student.new(student_params)
+    respond_to do |format|
+      if @student.save
+        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end   
+  end
+
+  def update
+    respond_to do |format|
+      if @student.update(student_params)
+        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @student.destroy
+    respond_to do |format|
+      format.html { redirect_to students_url, notice: 'Student was successfully destroyed.' }
+    end
+  end
+
 
   private
   def student_params
     params.require(:student).permit(:name, :course, :enrollment_status, :site, :school_year, :parent_name, :phone_number, :email_address, :live_address)
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
   end
 end
