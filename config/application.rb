78a0ad00427933216f50f9bdad61dee1468caa4c
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-28 15:06:17 +0900

    enums日本語化 enroll_statementのみ未反映、調査中 #1

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module GradurateApp
   class Application < Rails::Application
-    config.i18n.default_locale = :ja   
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
