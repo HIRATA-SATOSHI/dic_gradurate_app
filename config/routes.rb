a0eeb18936920430592d0da91c1044d84e10ab16
satoshi.hirata <s.hirata0507@gmail.com>
2020-11-29 22:29:28 +0900

    複数選択削除の実装中 #1

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'students/index'
   # get 'students/show'
   # get 'students/edit'
+  delete 'students/destroy_all'
   resources :students do
     collection do
       post :confirm
