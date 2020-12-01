86cb0032a470b4342ee0cc14070aedf19e6207a2
satoshi.hirata <s.hirata0507@gmail.com>
2020-12-01 14:04:27 +0900

    deviseinstallとletter_openerとdevise staff作成 #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :staffs
   # get 'students/new'
   # get 'students/index'
   # get 'students/show'
   # get 'students/edit'
+  root 'students#index'
   resources :students do
     collection do
       post :confirm
     end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
