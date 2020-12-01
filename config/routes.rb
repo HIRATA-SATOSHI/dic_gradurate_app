d2e1a04c74295b19c622908e7b4b93d025f22eea
satoshi.hirata <s.hirata0507@gmail.com>
2020-12-01 18:12:57 +0900

    Devise controller rootをログイン画面に設定完了#2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :staffs
+  # devise_for :staffs
   # get 'students/new'
   # get 'students/index'
   # get 'students/show'
   # get 'students/edit'
-  root 'students#index'
+  # root 'students#index'
   resources :students do
     collection do
       post :confirm
     end
   end
+  devise_scope :staff do
+    root "staffs/sessions#new"
+  end
+
+  devise_for :staffs, :controllers => {
+    sessions: 'staffs/sessions'
+  }
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
