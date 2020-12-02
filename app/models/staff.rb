03a9b7f4a69911acd6c5314f36afb2b740668cc9
satoshi.hirata <s.hirata0507@gmail.com>
2020-12-02 15:25:35 +0900

    staffバリデーション実装 #2

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,6 +1,7 @@
 class Staff < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, :department, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
